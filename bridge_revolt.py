d3392c4c1b677ab085aec08c8f9dc600861c70fa
green. <41323182+gweeeen@users.noreply.github.com>
2024-02-24 13:10:05 +0100

    better logging

--- bridge_revolt.py
+++ bridge_revolt.py
@@ -4,12 +4,27 @@ import asyncio
 import aiohttp
 import revolt
 import json
+from time import gmtime, strftime
 
 with open('config.json', 'r') as file:
     data = json.load(file)
 
 owner = data['owner']
 external_services = data['external']
+
+def log(type='???',status='ok',content='None'):
+    time1 = strftime("%Y.%m.%d %H:%M:%S", gmtime())
+    if status=='ok':
+        status = ' OK  '
+    elif status=='error':
+        status = 'ERROR'
+    elif status=='warn':
+        status = 'WARN '
+    elif status=='info':
+        status = 'INFO '
+    else:
+        raise ValueError('Invalid status type provided')
+    print(f'[{type} | {time1} | {status}] {content}')
 
 class Revolt(commands.Cog,name='Revolt Support'):
     """An extension that enables Unifier to support Revolt. Manages Revolt instance, as well as Revolt-to-Revolt and Revolt-to-Discord bridging.
@@ -18,7 +33,7 @@ class Revolt(commands.Cog,name='Revolt Support'):
     def __init__(self,bot):
         self.bot = bot
         if not 'revolt' in external_services:
-            raise RuntimeError('revolt is not listed as an external service in config.json.')
+            raise RuntimeError('revolt is not listed as an external service in config.json. More info: https://unichat-wiki.pixels.onl/setup-selfhosted/getting-started#installing-revolt-support')
         if not hasattr(self.bot, 'revolt_client'):
             self.bot.revolt_client = None
             self.bot.revolt_session = None
@@ -26,23 +41,23 @@ class Revolt(commands.Cog,name='Revolt Support'):
 
     class Client(revolt.Client):
         async def on_ready(self):
-            print('Revolt client booted!')
+            log('RVT','ok','Revolt client booted!')
 
         async def on_message(self, message):
             print(message.content)
 
     async def revolt_boot(self):
         if self.bot.revolt_client is None:
-            print('Syncing Revolt rooms...')
+            log('DAT','info','Syncing Revolt rooms...')
             for key in self.bot.db['rooms']:
                 if not key in list(self.bot.db['rooms_revolt'].keys()):
                     self.bot.db['rooms_revolt'].update({key: {}})
-                    print('Synced room '+key)
+                    log('DAT','ok','Synced room '+key)
             self.bot.db.save_data()
             async with aiohttp.ClientSession() as session:
                 self.bot.revolt_session = session
                 self.bot.revolt_client = self.Client(session, data['revolt_token'])
-                print('booting revolt client')
+                log('RVT','info','Booting Revolt client...')
                 await self.bot.revolt_client.start()
 
     @commands.command(hidden=True)
