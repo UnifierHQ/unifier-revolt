6da56fe605bfd7ed7e11403e5c6ff110c8cb606d
green. <41323182+gweeeen@users.noreply.github.com>
2024-02-25 19:37:39 +0100

    fix

--- bridge_revolt.py
+++ bridge_revolt.py
@@ -687,7 +687,8 @@ class Revolt(commands.Cog,name='<:revoltsupport:1211013978558304266> Revolt Supp
                 except:
                     current_color = 'Default'
                     embed_color = self.bot.colors.unifier
-                embed_color = f'{embed_color}'.replace('0x','#',1)
+                embed_color = f'{embed_color}'.replace('0x','',1)
+                embed_color = 'rgb('+str(tuple(int(embed_color[i:i + 2], 16) for i in (0, 2, 4)))+')'
                 embed = revolt.SendableEmbed(title='Your Revolt color', description=current_color, colour=embed_color)
                 await ctx.send(embeds=[embed])
             elif color == 'inherit':
