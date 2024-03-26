4c663097b28f9f71385e011a7f9e9790f5bd2858
green. <41323182+gweeeen@users.noreply.github.com>
2024-03-26 12:40:01 +0100

    fix

--- bridge_revolt.py
+++ bridge_revolt.py
@@ -158,9 +158,12 @@ class Revolt(commands.Cog,name='<:revoltsupport:1211013978558304266> Revolt Supp
                 return
             roomname = None
             for key in self.bot.db['rooms_revolt']:
-                if message.channel.id in str(self.bot.db['rooms_revolt'][key][message.server.id]):
-                    roomname = key
-                    break
+                try:
+                    if message.channel.id in str(self.bot.db['rooms_revolt'][key][message.server.id]):
+                        roomname = key
+                        break
+                except:
+                    continue
             if not roomname:
                 return
             t = time.time()
