ed36606ea46ba90be6fcfaf48f1a495cdb778181
green. <41323182+greeeen-dev@users.noreply.github.com>
2024-07-11 10:58:09 +0200

    revolt fix

--- revolt_check.py
+++ revolt_check.py
@@ -1,6 +1,6 @@
 async def check(bot):
     await bot.revolt_session.close()
     if hasattr(bot, 'platforms'):
-        bot['platforms'].pop('revolt')
+        bot.platforms.pop('revolt')
     del bot.revolt_client
     del bot.revolt_session
