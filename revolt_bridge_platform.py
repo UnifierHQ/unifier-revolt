97a54438ae0cd19b818b46b34e9fb8c11d6d6a38
green. <41323182+greeeen-dev@users.noreply.github.com>
2024-08-20 18:15:09 +0200

    username trimming fix

--- revolt_bridge_platform.py
+++ revolt_bridge_platform.py
@@ -243,8 +243,10 @@ class RevoltPlatform(platform_base.PlatformBase):
             name = special['bridge']['name']
             if len(name) > 32:
                 name = name[:-(len(name)-32)]
-            if 'emoji' in special['bridge'].keys():
-                name = name[:-2] + ' ' + special['bridge']['emoji']
+                if 'emoji' in special['bridge'].keys():
+                    name = name[:-2] + ' ' + special['bridge']['emoji']
+            else:
+                name = name + ' ' + special['bridge']['emoji']
             persona = revolt.Masquerade(
                 name=name,
                 avatar=special['bridge']['avatar'] if 'avatar' in special['bridge'].keys() else None,
